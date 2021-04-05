2f54daf78aa819999dd3ab6b590f0179d0347cb8
SaintPriest <81962981+SaintPriest@users.noreply.github.com>
2021-04-05 22:33:27 +0800

    Add files via upload

--- MapleServer2/Packets/GameEventPacket.cs
+++ MapleServer2/Packets/GameEventPacket.cs
@@ -14,8 +14,8 @@ namespace MapleServer2.Packets
             pWriter.WriteUnicodeString("StringBoard"); // type of event
             pWriter.WriteInt(40000); // event ID
             pWriter.WriteInt(0); // 0 = use string below. else, use an id in stringboardtext.xml
-            pWriter.WriteUnicodeString("Welcome! Please see our Github or Discord for updates and to report any bugs. discord.gg/fVZRdBN7Nd");
+            pWriter.WriteUnicodeString("MapleStory 2 edited by Diana, Game Version: v1.19p alpha 6. Follow me on discord.gg/E96qZNA");
             return pWriter;
-        }
+        } // Welcome! Please see our Github or Discord for updates and to report any bugs. discord.gg/fVZRdBN7Nd
     }
 }
