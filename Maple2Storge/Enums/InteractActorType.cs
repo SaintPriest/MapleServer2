e5aa124a6ee544bc06ca1d863a092a38352a47e1
SaintPriest <81962981+SaintPriest@users.noreply.github.com>
2021-04-10 18:18:11 +0800

    Add files via upload

---
+++ Maple2Storge/Enums/InteractActorType.cs
@@ -0,0 +1,10 @@
+﻿namespace Maple2Storage.Enums
+{
+    public enum InteractActorType : byte
+    {
+        Unknown = 0,
+        Binoculars = 2,
+        Extractor = 3,
+        Gathering = 6,
+    }
+}
